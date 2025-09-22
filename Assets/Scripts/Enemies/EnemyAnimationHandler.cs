e6732b535c8ae414351567698ce50869dd1b3a5c
MahanVarghai <mahanvarghai@gmail.com>
2025-09-22 23:59:01 +0330

    feat(Enemy): Add Idle animation to the enemy

--- Assets/Scripts/Enemies/EnemyAnimationHandler.cs
+++ Assets/Scripts/Enemies/EnemyAnimationHandler.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class EnemyAnimationHandler : MonoBehaviour
 {
     private Animator anim;
+    public bool IdleAnimation { get; set; } = false;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        IdleAnimationHandler();
+    }
     public void WalkAnimationHandler()
     {
         anim.SetBool(TagManagement.ENEMY_WALK_ANIMATION_BOOL, true);
+    }
+    public void IdleAnimationHandler()
+    {
+        if (IdleAnimation)
+        {
+            anim.SetBool(TagManagement.ENEMY_WALK_ANIMATION_BOOL, false);
+            anim.SetBool(TagManagement.ENEMY_IDLE_ANIMATION_BOOL, true);
+        }
+        else
+        {
+            anim.SetBool(TagManagement.ENEMY_IDLE_ANIMATION_BOOL, false);
+            anim.SetBool(TagManagement.ENEMY_IDLE_ANIMATION_BOOL, true);
+        }
     }
 }
--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -4,14 +4,15 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
-
+    private Transform playerLocation;
     private Rigidbody2D rb;
+    private EnemyAnimationHandler eHandler;
     private bool isRisigAnimationDone = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     private void FixedUpdate()
@@ -22,7 +23,19 @@ public class EnemyMovement : MonoBehaviour
     private void EnemyWalkHandler()
     {
         if (isRisigAnimationDone)
-            rb.linearVelocityX = Vector3.left.x * speed;
+        {
+            if(Vector3.Distance(transform.position, playerLocation.position) > 0.5f)
+            {
+                rb.linearVelocityX = Vector3.left.x * speed; return;
+                eHandler.IdleAnimation = false;
+            }
+            else
+            {
+                rb.linearVelocityX = 0f;
+                eHandler.IdleAnimation = true;
+            }
+
+        }
     }
     public void IsRisingAnimationDone() => isRisigAnimationDone = true;
 }
